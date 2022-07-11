ae60df689a68b450ed77f66cf603ee0ec13503d4
Anne <annels87@hotmail.com>
2022-07-11 11:16:06 +0200

    .

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -3,13 +3,12 @@ import emailjs from '@emailjs/browser';
 import styles from '../styles/Home.module.scss'
 
 const ContactUs = () => {
-  const form = useRef();
-
+ 
   const sendEmail = (e) => {
     e.preventDefault();
-    e.target.reset();
+   
 
-    emailjs.sendForm('service_7bbxv1w', 'template_3t3i0eb', form.current, '3fkWKlIXOTJ-6HhgN')
+    emailjs.sendForm('service_7bbxv1w', 'template_3t3i0eb', e.target, '3fkWKlIXOTJ-6HhgN')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -17,14 +16,17 @@ const ContactUs = () => {
       });
 
     alert("Din besked blev sendt")
+    e.target.reset();
   };
+
+
 
   return (
     <section>
     <h3>Send mig en besked via nedenstående formular:</h3>
-    <form ref={form} onSubmit={sendEmail}>
+    <form onSubmit={sendEmail}>
       <label htmlFor="first">Navn</label>
-      <input type="text" id="first" name="user_first" required />
+      <input type="text" id="first" name="user_name" required />
 
       <label htmlFor="email">Email</label>
       <input type="email" id="email" name="user_email" required />
